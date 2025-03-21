84ceada78075fa5819145fbb00a683e980c0ab10
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-03-21 16:41:51 +0100

    added to library

---
+++ brain/src/lib.rs
@@ -0,0 +1,7 @@
+pub mod math; // include the math/mod.rs
+pub mod network;
+
+pub use math::activation::{sigmoid, relu}; // re-export for easier use
+pub use math::matrix::{Matrix};
+pub use math::random::{RandomGenerator, RandomGeneratorConfig};
+pub use network::network::{Network};
