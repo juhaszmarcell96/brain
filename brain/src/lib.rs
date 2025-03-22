6d4991a98804699ab346d82a2c3a7de3f7686074
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-03-22 17:38:49 +0100

    small fix

--- brain/src/lib.rs
+++ brain/src/lib.rs
@@ -2,6 +2,6 @@ pub mod math; // include the math/mod.rs
 pub mod network;
 
 pub use math::activation::{sigmoid, relu}; // re-export for easier use
-pub use math::matrix::{Matrix};
+pub use math::matrix::Matrix;
 pub use math::random::{RandomGenerator, RandomGeneratorConfig};
-pub use network::network::{Network};
+pub use network::network::Network;
