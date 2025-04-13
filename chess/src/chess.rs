0b8a063bdb77320140de262ddae5f0eb5336d307
Marcell Juhasz <marcell.juhasz96@gmail.com>
2025-04-13 18:04:26 +0200

    values and names

--- chess/src/chess.rs
+++ chess/src/chess.rs
@@ -11,3 +11,37 @@
   a b c d e f g h
 
 */
+
+/*
+
+♚ -> white king   -> -6
+♛ -> white queen  -> -5
+♝ -> white bishop -> -4
+♞ -> white knight -> -3
+♜ -> white rook   -> -2
+♟ -> white pawn   -> -1
+. -> empty field  ->  0
+♙ -> black pawn   ->  1
+♙ -> black rook   ->  2
+♘ -> black knight ->  3
+♗ -> black bishop ->  4
+♕ -> black queen  ->  5
+♔ -> black king   ->  6
+
+*/
+
+#[derive(Debug)]
+enum Pieces {
+    WhitePawn,
+    WhiteRook,
+    WhiteKnight,
+    WhiteBishop,
+    WhiteQueen,
+    WhiteKing,
+    BlackPawn,
+    BlackRook,
+    BlackKnight,
+    BlackBishop,
+    BlackQueen,
+    BlackKing
+}
